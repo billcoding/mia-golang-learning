ad8753a8396bd352d28cbbcf2708d9d273424afb
wangmiao <wangmiao@tc.me>
2025-01-10 22:07:12 +0800

    feat: day1.1 & day1.2

---
+++ day1.2/main.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+1.2 Go的安装和环境配置
+
+我们这里说的安装是指，安装Go的SDK，SDK是Software Development Kit，软件开发工具包。
+
+Go SDK，访问官网https://go.dev 直接下载
+官网提供多种平台（操作系统）的SDK供下载
+按你的操作系统具体指定的就可以了。
+
+如何验证安装成功了没有。
+
+在cmd上直接输入命令：go version查看你安装Go SDK的版本信息。
+
+go version go1.23.4 windows/amd64
+*/
